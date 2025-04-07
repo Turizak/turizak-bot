2557c610ed99068ed77dd49734d0a91101b40140
Tom Slanda <jesus_slanda@yahoo.com>
2025-04-07 17:31:04 -0400

    switched to dynamic imports in deploy command (#16)

--- deploy-command.js
+++ deploy-command.js
@@ -16,15 +16,23 @@ for (const folder of commandFolders) {
   // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      commands.push(command.data.toJSON());
-    } else {
-      logger.log({
-        level: "warn",
-        message: `The command at ${filePath} is missing a required "data" or "execute" property`,
+    import(filePath)
+      .then((command) => {
+        if ("data" in command && "execute" in command) {
+          commands.push(command.data.toJSON());
+        } else {
+          logger.log({
+            level: "warn",
+            message: `The command at ${filePath} is missing a required "data" or "execute" property`,
+          });
+        }
+      })
+      .catch((error) => {
+        logger.log({
+          level: "error",
+          message: `Error importing ${filePath}: ${error}`,
+        });
       });
-    }
   }
 }
 
