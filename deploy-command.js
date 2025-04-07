36d4a80b3728575e3960ef0c486e559711907834
Tom Slanda <jesus_slanda@yahoo.com>
2025-04-07 17:21:32 -0400

    added .mjs to filter (#15)

--- deploy-command.js
+++ deploy-command.js
@@ -12,7 +12,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
   // Grab all the command files from the commands directory you created earlier
   const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js") || file.endsWith(".mjs"));
   // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
